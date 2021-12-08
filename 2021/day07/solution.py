59556a2baddc5ca150351998f0d755282c5b292e
jacobgnewman <jacobgnewman@icloud.com>
2021-12-07 20:56:04 -0800

    improved function

--- 2021/day07/solution.py
+++ 2021/day07/solution.py
@@ -1,3 +1,3 @@
 inp = [int(x) for x in open('2021/day07/input.txt').read().split(',')]
 print("P1:", min(map(lambda y: sum(map(lambda x: abs(x-y), inp)),range(max(inp)+1))))
-print("P2:", min(map(lambda y: sum(map(lambda x:sum(range(abs(x-y)+1)), inp)), range(max(inp)+1))))
+print("P2:", min(map(lambda y: sum(map(lambda x: abs(x-y)*(abs(x-y)+1)//2, inp)), range(max(inp)+1))))
