2a5af4a53a9f291d2632b7c9d9a1496e8b06cc69
jacobgnewman <jacobgnewman@icloud.com>
2021-12-07 18:55:07 -0800

    Shortened with nice functional

--- 2021/day07/solution.py
+++ 2021/day07/solution.py
@@ -1,18 +1,3 @@
 inp = [int(x) for x in open('2021/day07/input.txt').read().split(',')]
-from math import factorial
-
-def solve():
-    ans = find_fuel(inp, 0)
-    for i in range(1, max(inp) + 1):
-        temp = find_fuel(inp, i)
-        ans = temp if temp < ans else ans
-    return ans
-
-def find_fuel(subs, idx)-> int:
-    tot = 0
-    for pos in subs:
-        tot += sum(range(1,(abs(pos-idx)+1)))
-    return tot
-print(solve())
-    
-
+print("P1:", min(map(lambda y: sum(map(lambda x: abs(x-y), inp)),range(max(inp)+1))))
+print("P2:", min(map(lambda y: sum(map(lambda x:sum(range(abs(x-y)+1)), inp)), range(max(inp)+1))))
