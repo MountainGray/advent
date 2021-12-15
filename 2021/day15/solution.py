96d854a2adcce9cb620518846105a2802e6abb4e
jacobgnewman <jacobgnewman@icloud.com>
2021-12-14 23:09:48 -0800

    inneficient but first baby

---
+++ 2021/day15/solution.py
@@ -0,0 +1,65 @@
+inp = open('2021/day15/input.txt').read().split('\n')
+import sys
+sys.setrecursionlimit(1500)
+
+grid = [[int(x) for x in line]for line in inp]
+newgrid = [[-1 for _ in range(len(inp[0])*5)] for _ in range(len(inp)*5)]
+for j in range((len(inp)*5)):
+    for i in range((len(inp[0])*5)):
+        level = i//len(inp[0])+j//len(inp)
+        newgrid[j][i] = (grid[i%len(inp[0])][j%len(inp)] + level)
+for j in range((len(inp)*5)):
+    for i in range((len(inp[0])*5)):
+        while newgrid[j][i]>9:
+            newgrid[j][i] -= 9
+
+
+#for line in newgrid:
+    #print(line)
+
+
+
+cost = [[-1 for _ in line] for line in newgrid]
+grid = newgrid
+cost[0][0]=0
+
+#base filter
+for j in range(len(grid)):
+    for i in range(len(grid[0])):
+        if i +1 < len(grid[0]):
+            if cost[j][i+1] == -1 or cost[j][i] + grid[j][i+1] < cost[j][i+1]:
+                cost[j][i+1] = cost[j][i] + grid[j][i+1]
+        if i-1 >= 0:
+            if cost[j][i-1] == -1 or cost[j][i] + grid[j][i-1] < cost[j][i-1]:
+                cost[j][i-1] = cost[j][i] + grid[j][i-1]
+        if j+1 < len(grid):
+            if cost[j+1][i] == -1 or cost[j][i] + grid[j+1][i] < cost[j+1][i]:
+                cost[j+1][i] = cost[j][i] + grid[j+1][i]
+        if j-1 >= 0:
+            if cost[j-1][i] == -1 or cost[j][i] + grid[j-1][i] < cost[j-1][i]:
+                cost[j-1][i] = cost[j][i] + grid[j-1][i]
+
+
+changed = True
+while changed:
+    changed = False
+    for j in range(len(grid)):
+        for i in range(len(grid[0])):
+            if i +1 < len(grid[0]):
+                if cost[j][i+1] == -1 or cost[j][i] + grid[j][i+1] < cost[j][i+1]:
+                    cost[j][i+1] = cost[j][i] + grid[j][i+1]
+                    changed = True
+            if i-1 >= 0:
+                if cost[j][i-1] == -1 or cost[j][i] + grid[j][i-1] < cost[j][i-1]:
+                    cost[j][i-1] = cost[j][i] + grid[j][i-1]
+                    changed = True
+            if j+1 < len(grid):
+                if cost[j+1][i] == -1 or cost[j][i] + grid[j+1][i] < cost[j+1][i]:
+                    cost[j+1][i] = cost[j][i] + grid[j+1][i]
+                    changed = True
+            if j-1 >= 0:
+                if cost[j-1][i] == -1 or cost[j][i] + grid[j-1][i] < cost[j-1][i]:
+                    cost[j-1][i] = cost[j][i] + grid[j-1][i]
+                    changed = True
+
+print(cost[-1][-1])
