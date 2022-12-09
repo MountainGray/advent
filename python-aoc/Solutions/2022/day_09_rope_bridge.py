dcb31540eeee265aa79312811c56df39b7b2f816
jacobgnewman <jacobgnewman@icloud.com>
2022-12-08 22:02:06 -0800

    cleaned 9

--- python-aoc/Solutions/2022/day_09_rope_bridge.py
+++ python-aoc/Solutions/2022/day_09_rope_bridge.py
@@ -1,60 +1,46 @@
-from advent import get_input, submit
+from advent import get_input, solution_timer
 inp = get_input(2022, 9)
-#inp = """R 4
-#U 4
-#L 3
-#D 1
-#R 4
-#D 1
-#L 5
-#R 2""".splitlines()
 
-visited = {(0, 0)}
-#xh, yh = 0,0 
-#xt, yt = 0,0
+dirs = {"D": [0, -1], "U": [0, 1], "L": [-1, 0], "R": [1, 0]}
 
-points = [[0, 0] for i in range(10)]
-for i in inp:
-    i = i.split()
-    d, dist = i[0], int(i[1])
-    for i in range(dist):
-        if d == "D":    
-            points[0][1] -= 1
-        if d == "U":    
-            points[0][1] += 1
-        if d == "L":    
-            points[0][0] -= 1
-        if d == "R":    
-            points[0][0] += 1
-        # drag tail
-        for j in range(1, len(points)):
-            xh, yh = points[j-1]
-            xt, yt = points[j]
-            if (abs(xt-xh) >1 and yt==yh):
-                if xt > xh:
-                    points[j][0] -= 1
-                else:
-                    points[j][0] += 1
-            elif (abs(yt-yh) >1 and xt==xh):
-                if yt > yh:
-                    points[j][1] -= 1
-                else:
-                    points[j][1] += 1
-            elif (abs(xt-xh) >=1 and abs(yt-yh) >1) or (abs(xt-xh) >1 and abs(yt-yh) >=1):
-                if xt>xh:
-                    points[j][0] -= 1
-                elif xt<xh:
-                    points[j][0] += 1
-                
-                if yt>yh:
-                    points[j][1] -= 1
-                elif yt<yh:
-                    points[j][1] += 1
-            if j == len(points)-1:
-                visited.add((xt, yt))
+def solve(inp, knots):
+    visited = set()
+    points = [[0, 0] for i in range(knots+1)]
+    for i in inp:
+        i = i.split()
+        d, dist = i[0], int(i[1])
+        for i in range(dist):
+            points[0][1] += dirs[d][1]
+            points[0][0] += dirs[d][0]
+            # drag tail
+            for j in range(1, len(points)):
+                xh, yh = points[j-1]
+                xt, yt = points[j]
+                if max([abs(xt-xh), abs(yt-yh)]) >1:
+                    if xt>xh:
+                        points[j][0] -= 1
+                    elif xt<xh:
+                        points[j][0] += 1
+                    if yt>yh:
+                        points[j][1] -= 1
+                    elif yt<yh:
+                        points[j][1] += 1
+                if j == len(points)-1:
+                    visited.add((points[j][0], points[j][1]))
+    return len(visited)
+
+@solution_timer(2022, 9, 1)
+def part_one(inp):
+    return solve(inp, 1)
+
+@solution_timer(2022, 9, 2)
+def part_two(inp):
+    return solve(inp, 9)
         
-
-print(len(visited))
+if __name__ == "__main__":
+    input = get_input(2022, 9)
+    part_one(input)
+    part_two(input)
 
 #submit(2022, 9, 1, ans)
 #submit(2022, 9, 2, ans)
