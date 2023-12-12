ccfd19a72cd846f06421ab9fe73f0341ca7bc514
jacobgnewman <57055451+jacobgnewman@users.noreply.github.com>
2023-12-11 23:37:08 -0800

    d12 delta baby

--- python-aoc/Solutions/2023/template.py
+++ python-aoc/Solutions/2023/template.py
@@ -1,25 +1,33 @@
+year = 2023
+day = 12
+tp = f'''
 from advent import get_input, solution_timer, submit
 
 
-@solution_timer(2023, 5, 1)
+@solution_timer({year}, {day}, 1)
 def part_one(inp):
     pass
 
 
-@solution_timer(2023, 5, 2)
+@solution_timer({year}, {day}, 2)
 def part_two(inp):
     pass
 
 if __name__ == "__main__":
-    test = get_input(2023, 5, filename="test.txt")
-    print("Test:")
-    part_one(test)
-    part_two(test)
+    inp = get_input({year}, {day})
+    test = """""".splitlines()
+    if test != [""]:
+        part_one(test)
 
-    print("inp:")
-    inp = get_input(2023, 5)
     ans = part_one(inp)
-    submit(2023, 5, 1, ans)
-    ans = part_two(inp)
-    #submit(2023, 5, 2, ans)
+    submit({year}, {day}, 1, ans)
+    
+    exit()
 
+    if test != [""]:
+        part_two(test)
+    ans = part_two(inp)
+    submit({year}, {day}, 2, ans)
+'''
+
+print(tp)
